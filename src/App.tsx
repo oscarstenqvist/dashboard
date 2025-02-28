55685055210c7db282c308d08aa9581d9fa48c31
Oscar Stenqvist <stenqvistoscar@gmail.com>
2025-02-28 21:53:52 +0100

    Feat: added grids

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { styled, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+
+const StyledGridContainer = styled(Grid)`
+  height: 100vh;
+  width: 100vw;
+`;
+
+const StyledGrid = styled(Grid)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+`;
+
 function App() {
   return (
-    <>
-    </>
+    <StyledGridContainer container spacing={2} padding={2}>
+      <StyledGrid size={6}>
+        <Typography>Grid 1</Typography>
+      </StyledGrid>
+      <StyledGrid size={6}>
+        <Typography>Grid 2</Typography>
+      </StyledGrid>
+      <StyledGrid size={6}>
+        <Typography>Grid 3</Typography>
+      </StyledGrid>
+      <StyledGrid size={6}>
+        <Typography>Grid 4</Typography>
+      </StyledGrid>
+    </StyledGridContainer>
   )
 }
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import { GlobalStyles } from '@mui/material';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
 const noBodyMarginPadding = {
   body: {
